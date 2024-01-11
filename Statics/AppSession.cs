1c956964daaca7f12de7546e221ffa48f13b30ef
Stathor <146978592+WH0LEWHALE@users.noreply.github.com>
2024-01-11 17:45:40 +0300

    Add files via upload

---
+++ Statics/AppSession.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using TGMacro.Bases;
+
+namespace TGMacro.Statics
+{
+    public static class AppSession
+    {
+        public static event EventHandler LanguageChanged;
+        public static List<LanguageBase> Languages = new List<LanguageBase>();
+        public static LanguageBase _activeLanguage;
+        public static LanguageBase ActiveLanguage { get => _activeLanguage; set { _activeLanguage = value; LanguageChanged?.Invoke(null, null); } }
+
+        private static bool _enable = false;
+
+        public static bool Enable
+        {
+            get => _enable;
+            set
+            {
+                _enable = value;
+                EnableStatusChanged?.Invoke(null, null);
+            }
+        }
+        public static event EventHandler EnableStatusChanged;
+        public static string Version = Assembly.GetExecutingAssembly().GetName().Version.Major + "." + Assembly.GetExecutingAssembly().GetName().Version.Minor;
+        public static float BuildNumber = Assembly.GetExecutingAssembly().GetName().Version.Revision;
+        private static bool _topMost = false;
+        public static bool TopMost
+        {
+            get => _topMost;
+            set
+            {
+                _topMost = value;
+                TopMostStatusChanged?.Invoke(null, null);
+            }
+        }
+
+        public static event EventHandler TopMostStatusChanged;
+
+        public static CSInputs.ReadInput.InputListener InputListener;
+
+        public static List<MacroItem> Macros = new List<MacroItem>();
+    }
+}
